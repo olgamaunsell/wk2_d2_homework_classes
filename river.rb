f4ba158ee823ba8f82f042d7148a6cd5e6cf355f
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-11-28 19:54:36 +0000

    3rd test passed

--- river.rb
+++ river.rb
@@ -1,9 +1,10 @@
 class River
 
-  attr_reader :name
+  attr_reader :name, :fish_stock
 
-  def initialize(name)
+  def initialize(name, fish_stock)
     @name = name
+    @fish_stock = fish_stock
   end
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -4,11 +4,14 @@ require_relative("../river.rb")
 
 class TestRiver < MiniTest::Test
   def setup
-    @river = River.new("Forth")
+    @river = River.new("Forth", ["Goldie", "Tiddler", "Pinkie"])
   end
 
   def test_get_name
     assert_equal("Forth", @river.name)
   end
 
+  def test_get_fish_stock
+    assert_equal(["Goldie", "Tiddler", "Pinkie"], @river.fish_stock)
+  end
 end
